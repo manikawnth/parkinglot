c1c907ab46f4106ebf4c56cca3627f98cff14d87
Manikanth <x501636@corp.avisbudget.com>
2016-04-28 15:01:53 -0400

    Demo

--- client/js/app.js
+++ client/js/app.js
@@ -4,8 +4,8 @@
  * Description
  */
 (function() {
-    var app = angular.module('ParkingApp', ['ngMaterial','smart-table']);
-    
+    var app = angular.module('ParkingApp', ['ngMaterial', 'smart-table']);
+
     //Any configuration
     app.config(function($mdThemingProvider) {
         $mdThemingProvider.theme('default')
@@ -14,59 +14,74 @@
             .warnPalette('light-blue');
     });
 
-    //Parking lot values
-    app.value('lots',{
-        'D1':'A11',
-        'D2':'A22',
-        'D3':'B12'
-    })
+    /*    //Parking lot values
+        app.value('lots',{
+            'D1':'A11',
+            'D2':'A22',
+            'D3':'B12'
+        })*/
 
     //Parking lot factory
-    app.factory('parkingLot',['lots',function(lots){
-        console.log("Entered Parking lot factory");
-        console.log(lots);
+    app.factory('parkingLot', ['$http', function($http) {
         var parkinglot = {
-            lots :lots,
-            getLot : function(devid){
-                if(devid){
-                    return lots[devid];    
-                }                
+            lots: {},
+            devices: {},
+            getLots: function(id) {
+                return $http.get('/lot?id=' + id);
             },
-            saveLot : function(devid,lotid){
-                var existing_dev = this.getDev(lotid);
-                if(existing_dev){
-                   lots[existing_dev] = ''; 
+            getDevices: function(dev) {
+                return $http.get('/dev?id=' + dev);
+            },
+            getLot: function(devid) {
+                if (devid) {
+                    return this.lots[devid];
                 }
-                lots[devid] = lotid;
             },
-            getDev : function(lotid){
-                if(lotid){
-                    return _.invert(lots)[lotid];    
-                }                
+            saveLot: function(devid, lotid) {
+                return $http.post('/lot?lotid=' + lotid + '&' + 'devid=' + devid);
+            },
+            getDev: function(lotid) {
+                if (lotid) {
+                    return _.invert(this.lots)[lotid];
+                }
             }
         }
         return parkinglot;
     }])
 
     //Controller
-    app.controller('ParkingCtrl', ['$scope','parkingLot', function($scope,parkingLot) {
+    app.controller('ParkingCtrl', ['$scope', 'parkingLot', function($scope, parkingLot) {
         var pc = this;
-        pc.lots = parkingLot.lots;
         pc.errorMsg = '';
 
+        parkingLot.getDevices('all')
+            .then(function(resp) {
+                parkingLot.devices = resp.data;
+                pc.lot_devices = resp.data;
+            });
+
+        parkingLot.getLots('all')
+            .then(function(resp) {
+                parkingLot.lots = resp.data;
+                pc.lots = parkingLot.lots;
+            });
         //pc.selected_device is the device id number
         //pc.input_lot is the input lot number
-        
-        pc.assign_lot = function(){
+
+        pc.assign_lot = function() {
             pc.errorMsg = '';
             var devid = parkingLot.getDev(pc.input_lot);
-            if (devid){
+            if (devid) {
                 pc.errorMsg = "Parking Lot already assigned to device:" + devid;
+            } 
+            else {
+                parkingLot.saveLot(pc.selected_device, pc.input_lot)
+                    .then(function(resp) {
+                        parkingLot.lots = resp.data;
+                        pc.lots = parkingLot.lots;
+                    })
             }
-            else{
-                parkingLot.saveLot(pc.selected_device,pc.input_lot);
-            }
-            pc.selected_device = undefined;   
+            pc.selected_device = undefined;
             pc.input_lot = undefined;
         }
     }]);
--- server.js
+++ server.js
@@ -16,13 +16,48 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.get('/',function(req,res){
-	console.log("Entered home page");
-	res.sendFile('index.html',{root:__dirname + '/client'});
+app.get('/', function(req, res) {
+    console.log("Entered home page");
+    res.sendFile('index.html', { root: __dirname + '/client' });
 })
 
-
-
-app.listen(port,function(){
-	console.log("Server is listening on port " + port);
+app.get('/lot',function(req,res){
+	if(req.query.id == 'all'){
+		res.json(lots);
+	}
 })
+
+app.get('/dev',function(req,res){
+	if(req.query.id == 'all'){
+		res.json(lot_devices);
+	}
+})
+
+app.post('/lot',function(req,res){
+	var lotid = req.query.lotid;
+	var devid = req.query.devid;
+    
+    lots[devid] = lotid;
+    res.json(lots);
+
+})
+
+app.listen(port, function() {
+    console.log("Server is listening on port " + port);
+})
+
+var lot_devices = {
+	'00:1A:7D:DA:71:14':'pi2',
+	'60:1A:7D:DA:71:15':'pi3',
+	'80:1A:7D:DA:71:14':'pi4'
+}
+
+var vehicles = {
+	'A0:02:DC:51:49:F9' : '068543219'
+}
+
+var lots = {
+    '00:1A:7D:DA:71:14': 'A11',
+    '60:1A:7D:DA:71:15': 'A22',
+    '80:1A:7D:DA:71:14': 'B12'
+}
