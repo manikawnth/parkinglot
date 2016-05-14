600dfd2cfe3be52248d175b25149e0bc3e9ad4de
Manikanth <x501636@corp.avisbudget.com>
2016-05-14 12:43:44 -0400

    merged post

--- server.js
+++ server.js
@@ -21,71 +21,68 @@ app.get('/', function(req, res) {
     res.sendFile('index.html', { root: __dirname + '/client' });
 })
 
-app.get('/lot',function(req,res){
-	if(req.query.id == 'all'){
-		res.json(lots);
-	}
+app.get('/lot', function(req, res) {
+    if (req.query.id == 'all') {
+        res.json(lots);
+    }
 
 })
 
 
-app.get('/dev',function(req,res){
-	if(req.query.id == 'all'){
-		res.json(lot_devices);
-	}
+app.get('/dev', function(req, res) {
+    if (req.query.id == 'all') {
+        res.json(lot_devices);
+    }
 })
 
-//Pass Blue-tooth adddress and get the corresponding vehicle
-app.get('/veh',function(req,res){
-	var req_veh = req.body.veh; // This is an array
-	var resp_veh = {veh:[]}
-	req_veh.forEach(function(bdaddr){
-		var doc = {}
-		if (vehicles[bdaddr]){
-			doc.bdaddr = vehicles[bdaddr];
-			resp_veh.veh.push(doc);
-		}
-	})
-	resp.json(resp_veh);
-
-})
 
 //Get Blue-tooth adddresses and return only vehicles
-app.post('/veh',function(req,res){
-	if(req.query.id){
-		resp = {'veh':vehicles[req.query.id]};
-		res.json(resp);
-	}
+app.post('/veh', function(req, res) {
+    if (req.query.id) {
+        resp = { 'veh': vehicles[req.query.id] };
+        res.json(resp);
+    } else if {
+        var req_veh = req.body.veh; // This is an array
+        var resp_veh = { veh: [] }
+        req_veh.forEach(function(bdaddr) {
+            var doc = {}
+            if (vehicles[bdaddr]) {
+                doc.bdaddr = vehicles[bdaddr];
+                resp_veh.veh.push(doc);
+            }
+        })
+        resp.json(resp_veh);
+    }
 })
 
-app.post('/lot',function(req,res){
-	var lotid = req.query.lotid;
-	var devid = req.query.devid;
-	if(lotid && devid && lots[devid] ){
-	    lots[devid] = lotid;
-	}    
-	res.json(lots);		
+app.post('/lot', function(req, res) {
+    var lotid = req.query.lotid;
+    var devid = req.query.devid;
+    if (lotid && devid && lots[devid]) {
+        lots[devid] = lotid;
+    }
+    res.json(lots);
 
 })
 
-app.post('/checkin',function(req,res){
-	//console.log(req.body);	
-	var lotid = req.body.lotid;
-	var mva = req.body.mva;
-	var miles = req.body.miles;
-	var gas = req.body.gas
-	
-	checkin[lotid].mva = mva;
-	checkin[lotid].miles = miles;
-	checkin[lotid].gas = gas;
-	//console.log(checkin);
-	res.status(200).end();
+app.post('/checkin', function(req, res) {
+    //console.log(req.body);	
+    var lotid = req.body.lotid;
+    var mva = req.body.mva;
+    var miles = req.body.miles;
+    var gas = req.body.gas
+
+    checkin[lotid].mva = mva;
+    checkin[lotid].miles = miles;
+    checkin[lotid].gas = gas;
+    //console.log(checkin);
+    res.status(200).end();
 })
 
 
-app.get('/checkin',function(req,res){
-	var lotid = req.query.lotid;
-	res.json(checkin[lotid]);
+app.get('/checkin', function(req, res) {
+    var lotid = req.query.lotid;
+    res.json(checkin[lotid]);
 
 })
 app.listen(port, function() {
@@ -93,14 +90,14 @@ app.listen(port, function() {
 })
 
 var lot_devices = {
-	'00:1A:7D:DA:71:14':'LOT1',
-	'60:1A:7D:DA:71:15':'DUMMY2',
-	'80:1A:7D:DA:71:14':'DUMMY3'
+    '00:1A:7D:DA:71:14': 'LOT1',
+    '60:1A:7D:DA:71:15': 'DUMMY2',
+    '80:1A:7D:DA:71:14': 'DUMMY3'
 }
 
 var vehicles = {
-	'A0:02:DC:51:49:F9' : '068543219',
-	'D0:25:98:BD:C0:3B' : '894532103'
+    'A0:02:DC:51:49:F9': '068543219',
+    'D0:25:98:BD:C0:3B': '894532103'
 }
 
 var lots = {
@@ -110,5 +107,5 @@ var lots = {
 }
 
 var checkin = {
-	'00:1A:7D:DA:71:14' : { mva:' ' , miles:' ',gas:' '}
+    '00:1A:7D:DA:71:14': { mva: ' ', miles: ' ', gas: ' ' }
 }
